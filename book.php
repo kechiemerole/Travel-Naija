1b39ff9062a495b2d9ce0af8339f18ff5a70fe35
Kechi Emerole <kechiemerole@gmail.com>
2023-07-10 22:54:49 -0400

    test

--- book.php
+++ book.php
@@ -19,7 +19,7 @@
         $mail -> isSMTP();
         $mail -> Host = 'smtp.gmail.com';
         $mail -> SMTPAuth = true;
-        $mail -> Username = 'kechiemerole@gmail.com';
+        $mail -> Username = 'kechiemerolee@gmail.com';
         $mail -> Password = '';
         $mail -> SMTPSecure = "tls";
         $mail -> Port = '587';
