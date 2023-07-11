01572cecce752664ab9730e071ecdd45949d38f6
Kechi Emerole <kechiemerole@gmail.com>
2023-07-10 22:50:11 -0400

    test

--- book.php
+++ book.php
@@ -20,7 +20,7 @@
         $mail -> Host = 'smtp.gmail.com';
         $mail -> SMTPAuth = true;
         $mail -> Username = 'kechiemerole@gmail.com';
-        $mail -> Password = 'fnozzqlvpdnfmrdf';
+        $mail -> Password = '';
         $mail -> SMTPSecure = "tls";
         $mail -> Port = '587';
         $mail -> sentFrom = ('kechiemerole@gmail.com');
